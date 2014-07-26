dc22f56cba7a012d682040608d06f9de7259dc7f
LukeMcNemee <nemecl@seznam.cz>
2014-07-26 19:41:36 +0200

    random shuffle and moving files

--- RandomFileMover/src/randomfilemover/RandomFileMover.java
+++ RandomFileMover/src/randomfilemover/RandomFileMover.java
@@ -3,8 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package randomfilemover;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  *
@@ -16,7 +21,27 @@ public class RandomFileMover {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        File folder;
+        folder = new File("./");
+        File dir;
+        String path = "";        
+        int i = 0;
+        
+        File[] listOfFiles = folder.listFiles();
+        List<File> files = new ArrayList<>(Arrays.asList(listOfFiles));
+        Collections.shuffle(files);
+        
+        for (File nextFile : files) {
+            if(nextFile.getName().contains(".mp3")){
+                if(i % 10 == 0){
+                    path = Integer.toString(i/10);
+                    dir = new File(path);
+                    dir.mkdir();
+                }
+                i++;
+                nextFile.renameTo(new File(path + "/"+ nextFile.getName()));
+            }
+        }
     }
-    
+
 }
